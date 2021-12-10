710ea6a68a7519378fb9c08aedf557297da003dd
Roman Zhuzha <romazhuzha@gmail.com>
2021-12-10 22:09:56 +0100

    update day 3 code a bit

--- 2021/03_solution.py
+++ 2021/03_solution.py
@@ -35,11 +35,11 @@ def get_sum_in_position(numbers: list, position: int) -> int:
 def calculate_rating(numbers: list, bit: int) -> int:
     col_num = 0
     while len(numbers) > 1:
-        oxygen_generator_sum = get_sum_in_position(numbers, col_num)
-        if oxygen_generator_sum >= len(numbers)/2:
-            numbers = filter_numbers(numbers, col_num, abs(bit))
+        if get_sum_in_position(numbers, col_num) >= len(numbers)/2:
+            _bit = bit
         else:
-            numbers = filter_numbers(numbers, col_num, abs(bit - 1))
+            _bit = abs(bit - 1)
+        numbers = filter_numbers(numbers, col_num, _bit)
         col_num = col_num + 1
 
     return int(''.join(str(i) for i in numbers[0]), 2)
@@ -52,13 +52,13 @@ def one(numbers: list) -> int:
 
     for col_num in range(len(numbers[0])):
         if get_sum_in_position(numbers, col_num) >= len(numbers)/2:
-            gamma_rate_bits.append('1')
-            epsilon_rate_bits.append('0')
+            bit = 1
         else:
-            gamma_rate_bits.append('0')
-            epsilon_rate_bits.append('1')
+            bit = 0
+        gamma_rate_bits.append(abs(bit))
+        epsilon_rate_bits.append(abs(bit - 1))
 
-    return int(''.join(gamma_rate_bits), 2) * int(''.join(epsilon_rate_bits), 2)
+    return int(''.join(str(i) for i in gamma_rate_bits), 2) * int(''.join(str(i) for i in epsilon_rate_bits), 2)
 
 
 def two(numbers: list) -> int:
